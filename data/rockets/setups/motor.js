6165d509a043e95ca6864cbf914be384f8338ea6
thzero <thzero@users.noreply.github.com>
2023-09-06 17:57:03 -0500

    updates

--- data/rockets/setups/motor.js
+++ data/rockets/setups/motor.js
@@ -1,5 +1,9 @@
+import LibraryCommonUtility from '@thzero/library_common/utility/index.js';
+
 class RocketStepStageMotor {
 	constructor(index) {
+		this.id = LibraryCommonUtility.generateId();
+		this.rocketStageMotorId = null;
 		this.index = index;
 		this.motor = null;
 	}
