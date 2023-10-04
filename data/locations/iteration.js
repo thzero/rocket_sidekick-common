fcbc77362e15353b631ac90f1e5e4b45e88149d9
thzero <thzero@users.noreply.github.com>
2023-10-04 18:52:47 -0500

    fix

--- data/locations/iteration.js
+++ data/locations/iteration.js
@@ -2,8 +2,6 @@ import LibraryCommonUtility from '@thzero/library_common/utility/index.js';
 
 class LocationIteration {
 	constructor() {
-		super();
-		
 		this.id = LibraryCommonUtility.generateId();
 		this.address = {};
 		this.name = null;
